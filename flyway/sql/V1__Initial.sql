33fed0f8f48bcba0c10922e93da7c3b1b34ee20c
Alexandre Pacheco <alexandrespmg@hotmail.com>
2022-09-19 00:01:08 -0400

    feat: sql create table

--- flyway/sql/V1__Initial.sql
+++ flyway/sql/V1__Initial.sql
@@ -1,3 +1,18 @@
-CREATE TABLE MyTable (
-    MyColumn VARCHAR(100) NOT NULL
+CREATE TABLE OperationallyAvailableCapacityTW (
+    id SERIAL PRIMARY KEY,
+    loc INTEGER,
+    loc_zn TEXT,
+    loc_name TEXT,
+    loc_purp_desc TEXT,
+    loc_qti TEXT,
+    flow_ind TEXT,
+    dc INTEGER,
+    opc INTEGER,
+    tsq INTEGER,
+    oac INTEGER,
+    it TEXT,
+    auth_overrun_ind TEXT,
+    nom_cap_exceed_ind TEXT,
+    all_qty_avail TEXT,
+    qty_reason TEXT
 );
