883464abbcebec9027f9f3e36ff8ca81e1ce05b4
Alexandre Pacheco <alexandrespmg@hotmail.com>
2022-09-19 00:15:29 -0400

    feat: implement app to make the call

--- DownloadCsvOverHttp/Program.cs
+++ DownloadCsvOverHttp/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DownloadCsvOverHttp.repository;
+using DownloadCsvOverHttp.services;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting App...");
+
+var energyTransferService = new EnergyTransferService();
+
+var operationallyAvailableCapacity = await energyTransferService.GetOperationallyAvailableCapacityFromDateRange(
+    DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1));
+
+var validatedData = Validator.Validate(operationallyAvailableCapacity);
+
+var repository = new OperationallyAvailableCapacityRepository();
+
+await repository.Save(validatedData.valid);
+
+Console.WriteLine("App Finishing...");
