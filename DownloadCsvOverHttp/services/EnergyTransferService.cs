7e3a5ed63487f69abc5a3d8e6b5a1611de686ac4
Alexandre Pacheco <alexandrespmg@hotmail.com>
2022-09-19 00:13:59 -0400

    feat: energy service for download csv

---
+++ DownloadCsvOverHttp/services/EnergyTransferService.cs
@@ -0,0 +1,52 @@
+using System.Globalization;
+using CsvHelper;
+using DownloadCsvOverHttp.models;
+
+namespace DownloadCsvOverHttp.services;
+
+public class EnergyTransferService
+{
+    private readonly HttpClient _client = new();
+    private readonly string _baseUri = "https://twtransfer.energytransfer.com/ipost/capacity/operationally-available";
+
+    public Uri GetUri(DateTime date)
+    {
+        var uriBuilder = new UriBuilder(_baseUri);
+
+        uriBuilder.Query = $"?gasDay={date.Month.ToString("D2")}%2F{date.Day.ToString("D2")}%2F{date.Year}&cycle={Cycles.Final.GetHashCode()}" +
+                           $"&searchType=ALL&searchString=&locType=ALL&locZone=ALL&f=csv&extension=csv&asset=TW";
+
+        return uriBuilder.Uri;
+    }
+
+    public async Task<List<OperationallyAvailableCapacityTw>> GetOperationallyAvailableCapacity(Uri uri)
+    {
+        Console.WriteLine($"Starting download... URL: {uri}");
+
+        var streamTask = await _client.GetStreamAsync(uri);
+
+        using (var reader = new StreamReader(streamTask))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<OperationallyAvailableCapacityTw>();
+
+            Console.WriteLine("Download finished...");
+
+            return records.ToList();
+        }
+    }
+
+    public async Task<List<OperationallyAvailableCapacityTw>> GetOperationallyAvailableCapacityFromDateRange(DateTime initialRange, DateTime finalRange)
+    {
+        var operationallyAvailableCapacityTws = new List<OperationallyAvailableCapacityTw>();
+
+        for (DateTime date = initialRange; date <= finalRange; date = date.AddDays(1))
+        {
+            var operationallyAvailableCapacityTw = await GetOperationallyAvailableCapacity(GetUri(date));
+
+            operationallyAvailableCapacityTws.AddRange(operationallyAvailableCapacityTw);
+        }
+
+        return operationallyAvailableCapacityTws;
+    }
+}
